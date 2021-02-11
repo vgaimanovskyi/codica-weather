40d3f68835c4ccf80990809dc3f95b7c97ef2c51
vgaimanovskyi <vgaymanovskiy@gmail.com>
2021-02-11 16:34:42 +0200

    push dist

---
+++ dist/precache-manifest.79e45e8a0076a21410901b5b03d30d29.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a2282d453ca3b9d193",
+    "url": "/codica-weather/css/chunk-711388a2.b027ed78.css"
+  },
+  {
+    "revision": "448a96ea1d67f0b5e0ed",
+    "url": "/codica-weather/css/chunk-vendors.e4943a01.css"
+  },
+  {
+    "revision": "6307ad9f4d4fb84e9309",
+    "url": "/codica-weather/css/index.15bc9f56.css"
+  },
+  {
+    "revision": "3b595a0213ac590eab94735442ec163f",
+    "url": "/codica-weather/index.html"
+  },
+  {
+    "revision": "e6a2282d453ca3b9d193",
+    "url": "/codica-weather/js/chunk-711388a2.274fc72a.js"
+  },
+  {
+    "revision": "448a96ea1d67f0b5e0ed",
+    "url": "/codica-weather/js/chunk-vendors.d1a08eb3.js"
+  },
+  {
+    "revision": "6307ad9f4d4fb84e9309",
+    "url": "/codica-weather/js/index.07139427.js"
+  },
+  {
+    "revision": "c4ed4acbcf7d9c90c57ee8bbb3283674",
+    "url": "/codica-weather/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/codica-weather/robots.txt"
+  }
+]);
